461604cea6709aa725f157e66d90eba9f585e90f
Rangga Dimas <rangga7765@gmail.com>
2023-12-10 15:38:48 +0700

    Initial schema commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import db from '@/lib/supabase/db'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  console.log(db);
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
